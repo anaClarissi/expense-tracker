1c70d7ef7d739a6d797735fe610af5d198ce9d87
Ana Clarissi <anaclarissicruz@gmail.com>
2026-06-27 17:54:35 -0300

    feat: model Expense Class and Category Enum

---
+++ src/main/java/model/Category.java
@@ -0,0 +1,12 @@
+package model;
+
+public enum Category {
+
+    FOOD,
+    TRANSPORT,
+    HEALTH,
+    ENTERTAINMENT,
+    EDUCATION,
+    OTHER
+
+}
---
+++ src/main/java/model/Expense.java
@@ -0,0 +1,76 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Expense {
+
+    private Long id;
+
+    private String description;
+
+    private Double amount;
+
+    private Category category;
+
+    private LocalDate date;
+
+    public Expense () {};
+
+    public Expense(Long id, String description, Double amount, Category category, LocalDate date) {
+        this.id = id;
+        this.description = description;
+        this.amount = amount;
+        this.category = category;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Expense expense = (Expense) o;
+        return Objects.equals(id, expense.id) && category == expense.category;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, category);
+    }
+        
+}
